169037d667968156822183401df6eef2698e3fba
Alexander Huxel <Alexanderhuxel@gmail.com>
2021-10-09 00:25:14 +0200

    createt block for landingpage

--- web/app/themes/united-signals/resources/views/blocks/block-LandingPage.blade.php
+++ web/app/themes/united-signals/resources/views/blocks/block-LandingPage.blade.php
@@ -30,32 +30,32 @@ $buttonText = get_field('buttonText');
 $discordState = get_field('discordState');
 @endphp
 <div data-{{ $block['id'] }} class="{{ $block['classes'] }} block">
-@if ($discordState == 'Anzeigen')
+  @if ($discordState == 'Anzeigen')
 
-<img id="discord" onclick="openDicord()" class="w-5 h-5  transition-all ease-in-out duration-300 rounded-full hover:shadow-discord hover:cursor-pointer  absolute z-10 right-0 bottom-15" src="@asset('images/discord.svg')">
-<iframe id="iframe" class="absolute w-0 h-0 z-10  transition-all ease-in-out duration-300  right-6 bottom-15 " 
-src="https://discordapp.com/widget?id=894213761102123128&theme=dark&username="
-allowtransparency="true" 
-frameborder="0" 
-sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
-</iframe>
-
-
-@endif
+  <img id="discord" onclick="openDicord()"
+    class="w-5 h-5  transition-all ease-in-out duration-300 rounded-full hover:shadow-discord hover:cursor-pointer  absolute z-10 right-0 bottom-15"
+    src="@asset('images/discord.svg')">
+  <iframe id="iframe" class="absolute w-0 h-0 z-10  transition-all ease-in-out duration-300  right-6 bottom-15 "
+    src="https://discordapp.com/widget?id=894213761102123128&theme=dark&username=" allowtransparency="true"
+    frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
+  </iframe>
 
 
- <div class="flex">
-    <img class="w-1/2 h-1/2 aa-reflect" src="@asset('images/logo_plain.png')" />
-    <div class="flex flex-col relative w-2/3 mt-24">
-      <h1 class="text-h1 mb-6 font-roadrage text-white-200 flex flex-col">
+  @endif
+
+
+  <div class="flex flex-col lg:flex-row ">
+    <img class="w-1/2 h-1/2 ml-half lg:ml-0" src="@asset('images/logo_plain.png')" />
+    <div class=" transform scale-75 lg:scale-100 flex flex-col relative w-full md:w-2/3 lg:mt-32">
+      <h1 class=" text-h3 lg:text-h1 mb-6 font-roadrage text-white-200 flex flex-col">
         <span>{{ $firstRow }}</span>
         <span>{{ $secondRow }}</span>
       </h1>
-      <p class="text-body mb-5 font-quicksand text-white-200 w-56">
-       {{ $description }}
+      <p class="text-body mb-5 font-quicksand text-white-200 w-auto max-w-xs sm:w-56">
+        {{ $description }}
       </p>
-      <a href="#"  class="aa-customButton">
-       {{ $buttonText }}
+      <a href="#" class="aa-customButton">
+        {{ $buttonText }}
       </a>
     </div>
   </div>
