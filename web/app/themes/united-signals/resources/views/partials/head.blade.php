9d55fbf4df70687d698063a1b44aa7faa394c185
Alexander Huxel <Alexanderhuxel@gmail.com>
2021-10-09 00:25:55 +0200

    updated head section

--- web/app/themes/united-signals/resources/views/partials/head.blade.php
+++ web/app/themes/united-signals/resources/views/partials/head.blade.php
@@ -8,23 +8,22 @@
   <link rel="manifest" href="@asset('images/favicons/site.webmanifest')">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
-    rel="stylesheet"
-  />
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+    rel="stylesheet" />
   <link rel="mask-icon" href="@asset('images/favicons/safari-pinned-tab.svg')" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#ff0000">
   <meta name="theme-color" content="#ffffff">
-
+  <script src="https://embed.twitch.tv/embed/v1.js"></script>
   <style>
-   @font-face {
-        font-family: roadrage;
-        src: url("@asset('../resources/assets/fonts/Road_Rage.otf')");
-      }
-      @font-face {
-        font-family: deathrattle;
-        src: url("@asset('../resources/assets/fonts/deathrattlebb_reg.ttf')");
-      }
+    @font-face {
+      font-family: roadrage;
+      src: url("@asset('../resources/assets/fonts/Road_Rage.otf')");
+    }
+
+    @font-face {
+      font-family: deathrattle;
+      src: url("@asset('../resources/assets/fonts/deathrattlebb_reg.ttf')");
+    }
   </style>
 
   @php wp_head() @endphp
