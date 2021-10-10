c571a7b4f68443849e9d61a27a18af3141b095db
Alexander Huxel <Alexanderhuxel@gmail.com>
2021-10-10 15:29:13 +0200

    insert catch for impressum and datenschutz

--- web/app/themes/united-signals/resources/views/partials/content-page.blade.php
+++ web/app/themes/united-signals/resources/views/partials/content-page.blade.php
@@ -1,11 +1,24 @@
-<div class="px-2.5 sm:px-5 md:px-7 lg:px-10">
-
-
-    <div class="box mt-7 flex lg:flex-col flex-col">
-        @php
-        the_content()
-        @endphp
-    </div>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav">
-        <p>' . __('Pages:', 'sage'), 'after' => '</p>
-    </nav>']) !!}
+@if (get_the_title() == 'datenschutzerklaerung' || 'impressum')
+<div class="mt-10">
+    <style>
+        p,
+        h2,
+        h1,
+        h3,
+        h4,
+        li {
+            color: white !important;
+        }
+    </style>
+    @php
+    the_content();
+    @endphp
+</div>
+@else
+@php
+the_content()
+@endphp
+@endif
+{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav">
+    <p>' . __('Pages:', 'sage'), 'after' => '</p>
+</nav>']) !!}
