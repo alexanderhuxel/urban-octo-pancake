ff366644a8360a82f4864c63c4a6dcbf4be24a91
Alexander Huxel <Alexanderhuxel@gmail.com>
2021-10-10 15:27:02 +0200

    updatet footer

--- web/app/themes/united-signals/resources/views/partials/header.blade.php
+++ web/app/themes/united-signals/resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 <div class="wrapper w-full pt-6 px-2 md:px-5 xl:px-20">
     <header class="w-full border items-center flex justify-between ">
-        <a href="./index.html"> <img class="w-5 h-5" src="@asset('images/logo_plain.png')" alt="Logo der Webseite">
-            <a />
 
-            <button class="flex flex-col  items-center justify-around w-4 h-4 ">
-                <span class="w-4 h-0.75 bg-white-200"></span>
-                <span class="w-4 h-0.75  bg-white-200"></span>
-                <span class="w-4 h-0.75 bg-white-200"></span>
-            </button>
+        <img class="w-5 h-5" src="@asset('images/logo_plain.png')" alt="Logo der Webseite">
+
+        <button class="flex flex-col  items-center justify-around w-4 h-4 ">
+            <span class="w-4 h-0.75 bg-white-200"></span>
+            <span class="w-4 h-0.75  bg-white-200"></span>
+            <span class="w-4 h-0.75 bg-white-200"></span>
+        </button>
     </header>
 </div>
 <div class="wrapper w-full   px-2 md:px-5 xl:px-20">
-      <main class="mb-2.5 relative">
+    <main class="mb-2.5 relative">
