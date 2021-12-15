3e9006db55c0d746eda721ddc10ee476a80ebb02
alpakaAlex <alexander@alpha-alpaka.de>
2021-12-15 19:16:53 +0100

    fixed style bug and insert icon links

--- web/app/themes/artistgaming/resources/views/partials/header.blade.php
+++ web/app/themes/artistgaming/resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
 <div class="wrapper w-full pt-6 px-2 md:px-5 xl:px-20">
-    <header class="w-full  items-center flex justify-between ">
+    <header class="w-full  items-center flex justify-between relative">
 
         <a href="{{ get_home_url() }}">
-            <img class="w-5 h-5 z-20 absolute" src="@asset('images/logo_plain.png')" alt="Logo der Webseite">
+            <img class="w-4 h-4 z-20 absolute top-0" src="@asset('images/logo_plain.png')" alt="Logo der Webseite">
         </a>
 
         <button id="menu-button" class="flex flex-col  z-20 items-center justify-around w-4 h-4 ">
@@ -13,4 +13,4 @@
     </header>
 </div>
 <div class="wrapper    px-2 md:px-5 xl:px-20">
-    <main class="mb-2.5 test relative">
+    <main class="mb-2.5 mt-10 test relative">
