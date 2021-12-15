e6039a47896d6539153550efe5563ad1a0237514
alpakaAlex <alexander@alpha-alpaka.de>
2021-12-15 19:17:08 +0100

    update footer

--- web/app/themes/artistgaming/resources/views/partials/footer.blade.php
+++ web/app/themes/artistgaming/resources/views/partials/footer.blade.php
@@ -8,42 +8,47 @@
       {{ wp_nav_menu(array(
               'menu' => 'footerMenu')
           ) }}
-
-      <div class="flex items-center justify-center md:justify-between">
-        <ul class="list-none mb-0 items-center pl-0 flex ">
-          @if (have_rows('footerSlots','option'))
-          @while (have_rows('footerSlots','option'))
-          @php
-          the_row();
-          $link = get_sub_field('footerLink','option');
-          @endphp
-          @switch($link['url'])
-          @case(str_contains($link['url'], 'instagram'))
-          <li class="pl-0 mb-0">
-            <a target="_blank" rel="noopener noreferrer" href=" {{ $link['url'] }}">
-              <img class="w-4 h-4 mr-1 md:mr-2" src="@asset('images/social/instagram.png')"
-                alt="{{$link['title']}}" /></a>
-          </li>
-          @break
-          @case(str_contains($link['url'], 'facebook'))
-          <li class="pl-0 mb-0">
-            <a target="_blank" rel="noopener noreferrer" href="{{ $link['url'] }}">
-              <img class="w-4 h-4 mr-1 md:mr-2" src="@asset('images/social/facebook.png')"
-                alt="{{$link['title']}}" /></a>
-          </li>
-          @break
-          @case(str_contains($link['url'], 'twitch'))
-          <li class="pl-0 mb-0">
+      <li class="pl-0 mb-0">
+        <div class="flex items-center justify-center md:justify-between">
+          <ul class="list-none mb-0 items-center pl-0 flex ">
+            @if (have_rows('footerSlots','option'))
+            @while (have_rows('footerSlots','option'))
+            @php
+            the_row();
+            $link = get_sub_field('footerLink','option');
+            @endphp
+            @switch($link['url'])
+            @case(str_contains($link['url'], 'instagram'))
+            <li class="pl-0 mb-0">
+              <a target="_blank" rel="noopener noreferrer" href=" {{ $link['url'] }}">
+                <img class="w-4 h-4 mr-1 md:mr-2" src="@asset('images/social/instagram.png')"
+                  alt="{{$link['title']}}" /></a>
+            </li>
+            @break
+            @case(str_contains($link['url'], 'facebook'))
+            <li class="pl-0 mb-0">
+              <a target="_blank" rel="noopener noreferrer" href="{{ $link['url'] }}">
+                <img class="w-4 h-4 mr-1 md:mr-2" src="@asset('images/social/facebook.png')"
+                  alt="{{$link['title']}}" /></a>
+            </li>
+            @break
+            @case(str_contains($link['url'], 'twitch'))
             <a target="_blank" rel="noopener noreferrer" href=" {{ $link['url'] }} ">
               <img class=" w-4 h-4 mr-1 md:mr-2" src="@asset('images/social/twitch.png')"
                 alt="{{$link['title']}}" /></a>
-          </li>
-          @break
-          @endswitch
-          @endwhile
-          @endif
-        </ul>
-      </div>
+      </li>
+      @break
+      @endswitch
+      @endwhile
+      @endif
+      </ul>
     </div>
-  </footer>
+
+</div>
+<div class="text-white-100 lg:mr-11 mt-2">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+  from
+  <a class="text-purple-300 hover:text-white-300 transition-all" href="https://www.flaticon.com/"
+    title="Flaticon">www.flaticon.com</a>
+</div>
+</footer>
 </div>
