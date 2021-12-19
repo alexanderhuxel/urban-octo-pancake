d34e12848c7eeeb3f9a248e5b6724d9931199c44
Alexander Huxel <alexanderhuxel@gmail.com>
2021-12-19 16:51:00 +0100

    production build

--- web/app/themes/artistgaming/resources/views/layouts/app.blade.php
+++ web/app/themes/artistgaming/resources/views/layouts/app.blade.php
@@ -5,9 +5,10 @@
     <body id="body" class="flex relative flex-col overflow-x-hidden justify-center items-center bg-black-100">
         {{ wp_nav_menu(array('menu' => 'frontpageMenu')) }}
         <div class="overflow-x-hidden md:overflow-x-visible relative">
-            <div class="barba-animation"></div>
-            <div id="barba-wrapper" data-barba="wrapper">
-                <div class="barba-container" data-barba="container">
+            <div class="barba-container" data-barba="container">
+                <div class="barba-animation"></div>
+                <div id="barba-wrapper" data-barba="wrapper">
+
                     <div id="body-classes" class="common">
 
                         @php do_action('get_header') @endphp
